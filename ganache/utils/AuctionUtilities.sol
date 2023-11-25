e21b1e246c826040c2c5c67776329f3b6b7fc413
Ryan.Nguyen <Ryan.Nguyen@avepoint.com>
2023-11-25 15:00:00 +0700

    [new]: Add AuctionUtilities contract

--- ganache/utils/AuctionUtilities.sol
+++ ganache/utils/AuctionUtilities.sol
@@ -5,4 +5,11 @@ library AuctionUtilities {
   function getOffsetBlockNum() external pure returns (uint256) {
     return 0;
   }
+  function getRevealDurationInBlockUnit() external pure returns (uint256) {
+    // Should be at least 30m. Here value is returned for testing only 
+    return 1;
+  }
+  function getAverageTimePerBlock() external pure returns (uint256) {
+    return 0;
+  }
 }
